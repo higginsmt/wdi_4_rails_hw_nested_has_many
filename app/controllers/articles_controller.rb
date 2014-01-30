a4ea033c749cd81d6ce9ae7b0972dcdd95ff5a9a
Matt Higgins <higginsmt@gmail.com>
2014-01-30 08:49:53 -0500

    some more stuff is working, I think I understand it now but I'm going to run out of time

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,36 @@ class ArticlesController < ApplicationController
 	def index
     @articles = Article.all
   end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render  :new
+    end
+  end
+
+  def edit
+    @article = Article.find([:id])
+  end
+
+  def update
+   if  @bookmark.update_attributes(bookmark_params)
+    redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to action: :index
+  end
+
+
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,8 @@ class CommentsController < ApplicationController
 	def index
 		@comments = Comment.all
 	end
+
+	def show
+		@comment = Comment.find(params[:id])
+  end
 end
